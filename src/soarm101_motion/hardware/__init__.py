8abbf3ff83ab4b5dce7fdb1aca7baf510fb1d9ba
AgenticForge-Labs <larry.york@agenticforgelabs.com>
2026-07-28 20:24:25 -0400

    feat: export one-time motor setup

--- src/soarm101_motion/hardware/__init__.py
+++ src/soarm101_motion/hardware/__init__.py
@@ -1,7 +1,14 @@
-"""Hardware backends."""
+"""Hardware backends and one-time motor setup."""
 
 from soarm101_motion.hardware.base import SO101HardwareBackend
 from soarm101_motion.hardware.feetech import FeetechBackend
+from soarm101_motion.hardware.setup import FeetechMotorSetup, MotorSetupResult
 from soarm101_motion.hardware.simulation import SimulationBackend
 
-__all__ = ["FeetechBackend", "SO101HardwareBackend", "SimulationBackend"]
+__all__ = [
+    "FeetechBackend",
+    "FeetechMotorSetup",
+    "MotorSetupResult",
+    "SO101HardwareBackend",
+    "SimulationBackend",
+]
