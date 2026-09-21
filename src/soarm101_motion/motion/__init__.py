5ac027df2ddf17e0fdfc6647788512a53ce3d308
AgenticForge Labs <info@agenticforgelabs.com>
2026-09-21 15:38:49 -0400

    Export streaming state

--- src/soarm101_motion/motion/__init__.py
+++ src/soarm101_motion/motion/__init__.py
@@ -1,6 +1,6 @@
 """Motion planning and execution."""
 
-from soarm101_motion.motion.controller import MotionHandle, PlannedPath, RecordedPlan
+from soarm101_motion.motion.controller import JointStreamState, MotionHandle, PlannedPath, RecordedPlan
 from soarm101_motion.motion.managed_controller import MotionController
 
-__all__ = ["MotionController", "MotionHandle", "PlannedPath", "RecordedPlan"]
+__all__ = ["JointStreamState", "MotionController", "MotionHandle", "PlannedPath", "RecordedPlan"]
