a850e0724537f822b9e3197a7be8f0eedd83580b
AgenticForge Labs <info@agenticforgelabs.com>
2026-09-21 13:47:03 -0400

    Export recorded motion plan

--- src/soarm101_motion/motion/__init__.py
+++ src/soarm101_motion/motion/__init__.py
@@ -1,6 +1,6 @@
 """Motion planning and execution."""
 
-from soarm101_motion.motion.controller import MotionHandle, PlannedPath
+from soarm101_motion.motion.controller import MotionHandle, PlannedPath, RecordedPlan
 from soarm101_motion.motion.managed_controller import MotionController
 
-__all__ = ["MotionController", "MotionHandle", "PlannedPath"]
+__all__ = ["MotionController", "MotionHandle", "PlannedPath", "RecordedPlan"]
